3231534e50e52251cead3627e13765b1acc790a9
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-08 01:28:43 +0530

    Different type of constructor

--- src/App.java
+++ src/App.java
@@ -2,7 +2,35 @@ public class App  {
     public static void main(String[] args) throws Exception{
         //System.out.println("Hello, World");
         //Default ctor
-        Student A = new Student();
-        Student B = new Student();
+//        Student A = new Student();
+//        A.id=1;
+//        A.age=14;
+//        A.name="Vaibhav";
+//        A.nos=7;
+//        System.out.println(A.name);
+//        System.out.println(A.age);
+//        System.out.println(A.id);
+//        System.out.println(A.nos);
+//
+//        A.bunk();
+//        A.study();
+//        A.sleep();
+
+        Student B = new Student(1,12, "Vaibhav");
+        System.out.println(B.name);
+       System.out.println(B.age);
+      System.out.println(B.id);
+       System.out.println(B.nos);
+
+       //COPY CONSTRUCTOR
+
+        Student C = new Student( B);
+        System.out.println(C.name);
+        System.out.println(C.age);
+        System.out.println(C.id);
+        System.out.println(C.nos);
     }
-}
+    }
+
+
+
--- src/Student.java
+++ src/Student.java
@@ -10,16 +10,32 @@ public class Student{
         System.out.println("Default Constructor");
     }
 
+    //parameterised ctor
+    public Student(int id , int age, String name){
+        System.out.println("Student parameterised ctor called");
+        this.id=id;
+        this.name=name;
+        this.age=age;
+    }
+
+    //copy constructor
+    public Student(Student copy){
+        System.out.println("Student copy ctor called");
+        this.id=copy.id;
+        this.name= copy.name;
+        this.age= copy.age;
+    }
+
     //Method /Behavior
     public void study(){
-        System.out.println(name + "studying");
+        System.out.println(name + " studying");
     }
     public void sleep() {
-        System.out.println(name + "sleeping");
+        System.out.println(name + " sleeping");
     }
 
 
         public void bunk(){
-            System.out.println(name + "bunking");
+            System.out.println(name + " bunking");
         }
     }
