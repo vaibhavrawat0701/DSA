122fb24873a1135f16e69104d25d617b53d625dc
Vaibhav Rawat <vaibhavrawat0701@gmail.com>
2026-09-24 01:39:47 +0530

    Add Arrays DSA project

---
+++ ARRAYS/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main (String[] args){
+
+        //Q: store a roll number
+        int a = 9;
+
+        //Q: Store a preson's name
+        String name = "Vaibhav Singh Rawat";
+
+        //Q: store 5 roll numbers
+        int rno1=34;
+        int rno2=55;
+        int rno3=36;
+
+
+        //syntax
+        //datatype[] variable_name = new datatype[size];
+
+        //store 5 roll no:
+        int[] rnos = new int[5];
+
+        // or directly
+        int[] rnos2={32,23,5,1,24};
+
+    }
+}
